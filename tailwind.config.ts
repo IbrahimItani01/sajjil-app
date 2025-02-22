ce451abd1b73cd0517e849b541131a3fc9ad792c
IbrahimItani01 <i.itani2002@gmail.com>
2025-02-22 13:56:12 +0200

    css<>configued pre-set font sizes to use

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#4A90E2",
-        accent: "#F5A623",
-        background: "#F9F9F9",
-        text: "#333333",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#4A90E2",
+				accent: "#F5A623",
+				background: "#F9F9F9",
+				text: "#333333",
+			},
+			fontSize: {
+				h1: "64px",
+				h2: "40px",
+				h3: "24px",
+				subtitle: "24px",
+				body: "16px",
+				small: "14px",
+				preTitle: "10px",
+				button: "10px",
+				link: "16px",
+			},
+			fontFamily: {
+				poppins: "var(--font-poppins)",
+				inter: "var(--font-inter)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
