4e5e866a4727b0dd244151c26289d29fdd40770a
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-22 16:58:47 +0200

    utils<>created an is validEmail utiil function to handle error display in auth form

---
+++ lib/utils/auth.utils.ts
@@ -0,0 +1,37 @@
+import { AuthForm } from "../interfaces/auth.interfaces";
+
+export const isValidEmail = (email: string) => {
+	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
+type valiateFormParams = {
+	isLogin: boolean;
+	form: AuthForm;
+	setError: (error: string) => void;
+};
+export const validateForm = ({
+	isLogin,
+	form,
+	setError,
+}: valiateFormParams) => {
+	if (
+		!form.email ||
+		!form.password ||
+		(!isLogin && (!form.name || !form.confirmPassword))
+	) {
+		setError("All fields are required.");
+		return false;
+	}
+
+	if (!isValidEmail(form.email)) {
+		setError("Invalid email format.");
+		return false;
+	}
+
+	if (!isLogin && form.password !== form.confirmPassword) {
+		setError("Passwords do not match.");
+		return false;
+	}
+    return true;
+	setError("");
+};
