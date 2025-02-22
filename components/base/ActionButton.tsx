545706c0e5a6cede70b434d43cac6a97293daf4d
IbrahimItani01 <i.itani2002@gmail.com>
2025-02-22 14:00:02 +0200

    components<>designed the ActionButton component structure and props' injection

--- components/base/ActionButton.tsx
+++ components/base/ActionButton.tsx
@@ -21,5 +21,15 @@ const ActionButton = ({
 			route.push("/auth");
 		}
 	};
+	return (
+		<div
+			onClick={!forLanding ? onPress : handleNavigate}
+			className='bg-accent font-inter text-black font-semibold px-8 py-3 rounded-full cursor-pointer 
+            hover:scale-105 active:scale-95 transform transition duration-150'
+		>
+			{title}
+		</div>
+	);
+};
 
 export default ActionButton;
