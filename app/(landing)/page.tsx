92b51696e02e064b44e5108b757bdcee12fadeca
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-22 16:17:08 +0200

    pages<>fixed text height

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,18 +2,19 @@ import ActionButton from "@/components/base/ActionButton";
 import React from "react";
 
 const page = () => {
-	
 	return (
 		<div className='h-[100vh] w-full flex flex-col items-center justify-center gap-10'>
 			<div className='flex flex-col items-center gap-2'>
-				<h1 className='text-h1 font-poppins font-bold'>Welcome to Sajjil 👋</h1>
+				<h1 className='text-h2 font-poppins w-full font-bold'>
+					Welcome to Sajjil 👋
+				</h1>
 				<p className='text-h3 font-inter font-semibold'>
 					A revolution in task management
 				</p>
 			</div>
 			<ActionButton
 				title='Continue'
-                forLanding
+				forLanding
 			/>
 		</div>
 	);
