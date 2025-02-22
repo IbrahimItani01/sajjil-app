11d33e269f66a5a58e7e704fe8291a6e54bb5885
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-23 01:13:52 +0200

    components<>desigend and structured the addTaskForm component

--- components/dashboard/AddTaskForm.tsx
+++ components/dashboard/AddTaskForm.tsx
@@ -58,7 +58,63 @@ const AddTaskForm = ({
 	};
 
 	return (
-		
+		<div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
+			<div className='p-6 border-2 border-accent bg-white rounded-2xl relative w-96'>
+				<button
+					className='absolute top-2 right-2 text-gray-500 hover:text-gray-800'
+					onClick={toggleAddTask}
+				>
+					<XCircle size={24} />
+				</button>
+
+				<h2 className='text-xl font-semibold mb-4'>
+					{initialTask ? "Edit Task" : "Add New Task"}
+				</h2>
+
+				<CustomInput
+					label='Title'
+					type='text'
+					placeholder='Task title'
+					name='title'
+					value={taskForm.title}
+					onChange={handleChange}
+				/>
+
+				<CustomInput
+					label='Description'
+					type='text'
+					placeholder='Buy eggs'
+					name='description'
+					value={taskForm.description}
+					onChange={handleChange}
+				/>
+
+				<div className='mt-4'>
+					<label className='block text-sm font-medium text-gray-700'>
+						Date
+					</label>
+					<input
+						type='date'
+						name='date'
+						value={taskForm.date}
+						onChange={handleChange}
+						className='mt-1 p-2 border rounded w-full'
+					/>
+				</div>
+
+				<PrioritySelector
+					value={taskForm.priority}
+					onChange={handlePriorityChange}
+				/>
+
+				<div className='mt-6 w-fit mx-auto'>
+					<ActionButton
+						title={initialTask ? "Save Changes" : "Add Task"}
+						onPress={handleSubmit}
+					/>
+				</div>
+			</div>
+		</div>
 	);
 };
 
