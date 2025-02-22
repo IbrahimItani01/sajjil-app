a828ea92344b9b62be6ba8c2a4a0320918da16ad
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-22 22:56:41 +0200

    pages<>desigend all tasks page

--- app/dashboard/(all)/page.tsx
+++ app/dashboard/(all)/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+"use client";
 
-const page = () => {
-  return (
-    <div>
-     <h1>all tasks</h1>
-    </div>
-  )
-}
+import TaskCard from "@/components/dashboard/TaskCard";
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/store";
 
-export default page
+const Page = () => {
+	const tasks = useSelector((state: RootState) => state.tasks);
+
+	const sortedTasks = tasks.slice().sort((a, b) => {
+		// First, compare by date
+		const dateComparison =
+			new Date(a.date).getTime() - new Date(b.date).getTime();
+		if (dateComparison !== 0) return dateComparison;
+
+		// Then, compare by priority (high to low)
+		const priorityOrder = { "!!!": 3, "!!": 2, "!": 1 };
+		return priorityOrder[b.priority] - priorityOrder[a.priority]; // Reverse order for descending priority
+	});
+
+	return (
+		<div className='flex flex-col gap-3'>
+			{sortedTasks.map((task, index) => (
+				<TaskCard
+					key={index}
+					title={task.title}
+					description={task.description}
+					priority={task.priority}
+					date={task.date}
+					completed={task.completed}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default Page;
