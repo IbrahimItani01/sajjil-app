be3c35046549382198e39f7b8a012e207dad68cc
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-22 22:56:24 +0200

    pages<>designed the completed tasks page file

--- app/dashboard/completed/page.tsx
+++ app/dashboard/completed/page.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+"use client";
 
-const page = () => {
-  return (
-    <div>
-      all completed tasks
-    </div>
-  )
-}
+import TaskCard from "@/components/dashboard/TaskCard";
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/store";
 
-export default page
+const Page = () => {
+	const tasks = useSelector((state: RootState) => state.tasks);
+
+	const completedTasks = tasks.filter((task) => task.completed);
+
+	const sortedCompletedTasks = completedTasks.slice().sort((a, b) => {
+		const dateComparison =
+			new Date(a.date).getTime() - new Date(b.date).getTime();
+		if (dateComparison !== 0) return dateComparison;
+
+		const priorityOrder = { "!!!": 3, "!!": 2, "!": 1 };
+		return priorityOrder[a.priority] - priorityOrder[b.priority];
+	});
+
+	return (
+		<div className='flex flex-col gap-3'>
+			{sortedCompletedTasks.length > 0 ? (
+				sortedCompletedTasks.map((task, index) => (
+					<TaskCard
+						key={index}
+						title={task.title}
+						description={task.description}
+						priority={task.priority}
+						date={task.date}
+						completed={task.completed}
+					/>
+				))
+			) : (
+				<p className='text-gray-500 text-center'>No completed tasks.</p>
+			)}
+		</div>
+	);
+};
+
+export default Page;
