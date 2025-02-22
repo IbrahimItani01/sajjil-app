a65896d929c1adcd8d9de80494f31ebbd9556422
IbrahimItani01 <i.itani2002@gmail.com>
2025-02-22 14:11:07 +0200

    components<>designed and structured the tsx of the AuthForm component in a dynamic way

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -31,4 +31,75 @@ const AuthForm = () => {
 		});
 	};
 
+	return (
+		<div className='w-full h-[100vh] flex items-center justify-center'>
+			<div className='w-[50%] mx-auto flex flex-col gap-10 border border-accent rounded-3xl p-5'>
+				<div className='flex flex-col items-center gap-2'>
+					<Image
+						src='./logo.svg'
+						alt='logo'
+						width={100}
+						height={100}
+					/>
+					<h1 className='text-h2 font-poppins font-semibold'>
+						{isLogin ? "Welcome Back!" : "Create an Account"}
+					</h1>
+				</div>
+				<div className='flex flex-col gap-4'>
+					{!isLogin && (
+						<CustomInput
+							type='text'
+							label='Name'
+							name='name'
+							placeholder='John Doe'
+							value={form.name}
+							onChange={handleChange}
+						/>
+					)}
+					<CustomInput
+						type='email'
+						label='Email'
+						name='email'
+						placeholder='example@domain.com'
+						value={form.email}
+						onChange={handleChange}
+					/>
+					<CustomInput
+						type='password'
+						label='Password'
+						name='password'
+						placeholder='********'
+						value={form.password}
+						onChange={handleChange}
+					/>
+					{!isLogin && (
+						<CustomInput
+							type='password'
+							label='Confirm Password'
+							name='confirmPassword'
+							placeholder='********'
+							value={form.confirmPassword}
+							onChange={handleChange}
+						/>
+					)}
+				</div>
+				<div className='w-fit mx-auto'>
+					<ActionButton title={isLogin ? "Login" : "Register"} />
+				</div>
+				<div className='w-fit mx-auto text-sm font-inter'>
+					<p>
+						{isLogin ? "Don't have an account?" : "Already have an account?"}
+						<span
+							className='underline font-medium hover:text-accent cursor-pointer'
+							onClick={handleSwitch}
+						>
+							{isLogin ? " Register" : " Login"}
+						</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
 export default AuthForm;
