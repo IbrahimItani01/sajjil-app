ae7c2551ba209f29cbf7b6f82d622bd17137283f
IbrahimItani01 <i.itani2002@gmail.com>
2025-02-22 14:07:38 +0200

    components<>declared AuthForm compoennt to handl auth logic and navigation

---
+++ components/AuthForm.tsx
@@ -0,0 +1,9 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import CustomInput from "./base/CustomInput";
+import ActionButton from "./base/ActionButton";
+
+const AuthForm = () => {};
+
+export default AuthForm;
