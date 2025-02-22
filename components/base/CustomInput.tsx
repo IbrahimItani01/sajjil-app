37e367f4564ef041e6114ec299ee558c640b58ca
IbrahimItani01 <i.itani2002@gmail.com>
2025-02-22 14:06:39 +0200

    components<>desigend the input tag in CustomInput component by spreading all input base attributes and custom design

--- components/base/CustomInput.tsx
+++ components/base/CustomInput.tsx
@@ -9,6 +9,10 @@ const CustomInput = ({ label, className, ...rest }: CustomInputProps) => {
   return (
     <div className="flex flex-col gap-1">
       {label && <label className="font-inter">{label}</label>}
+      <input
+        className={`border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-accent text-black font-inter ${className}`}
+        {...rest}
+      />
     </div>
   );
 };
