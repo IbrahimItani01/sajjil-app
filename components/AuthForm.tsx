8c8a4a308f0a148f4de216be5ee98e037920dbab
IbrahimItani01 <i.itani2002@gmail.com>
2025-02-22 14:09:01 +0200

    components<> declared states in AuthForm to handle logina nd register ui switching and form object input values

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,6 +4,13 @@ import React, { useState } from "react";
 import CustomInput from "./base/CustomInput";
 import ActionButton from "./base/ActionButton";
 
-const AuthForm = () => {};
+const AuthForm = () => {
+	const [isLogin, setIsLogin] = useState(true);
+	const [form, setForm] = useState({
+		name: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
 
 export default AuthForm;
