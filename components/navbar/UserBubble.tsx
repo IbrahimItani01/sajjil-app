03a9e499be217782434d98645bb84de343d822a1
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-23 23:25:44 +0200

    components<>re-engineered teh user bubble to display user name

--- components/navbar/UserBubble.tsx
+++ components/navbar/UserBubble.tsx
@@ -1,11 +1,16 @@
 import { User } from "lucide-react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store"; // Adjust the import based on your actual store file
 
 const UserBubble = ({ className }: { className?: string }) => {
-	// TODO: handle user name state
+	// Access the user data from the Redux store
+	const user = useSelector((state: RootState) => state.auth.user?.username);
+
+
 	return (
 		<div className={`flex items-center bg-gray-200 text-gray-800 px-3 py-1 rounded-full space-x-2 cursor-default ${className}`}>
 			<User size={20} />
-			<span className='text-sm font-medium font-poppins'>John Doe</span>
+			<span className='text-sm font-medium font-poppins'>{user}</span>
 		</div>
 	);
 };
