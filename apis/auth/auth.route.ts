65a0d2d6395f4d52e614798d1f7805e3643a967d
IbrahimItani01 <i.itan2002@gmail.com>
2025-02-23 22:53:07 +0200

    routes<>created functions to call auth apis

---
+++ apis/auth/auth.route.ts
@@ -0,0 +1,32 @@
+import { AuthForm } from "@/lib/interfaces/auth.interfaces";
+import axios from "axios";
+import { API_URL } from "../main";
+
+export const registerUser = async ({ name, email, password }: AuthForm) => {
+	try {
+		const response = await axios.post(`${API_URL}/auth/register`, {
+			name,
+			email,
+			password,
+		});
+
+		return response.data;
+	} catch (error) {
+		console.error("Registration Error:", error);
+		throw error;
+	}
+};
+
+export const loginUser = async ({ email, password }: AuthForm) => {
+    try {
+		const response = await axios.post(`${API_URL}/auth/login`, {
+			email,
+			password,
+		});
+
+		return response.data;
+	} catch (error) {
+		console.error("Registration Error:", error);
+		throw error;
+	}
+};
